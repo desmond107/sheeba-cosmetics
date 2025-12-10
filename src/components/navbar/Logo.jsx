57bc743af0325fa9eb90cac8eea2ddc113903469
Desmond Kinoti <desmond.kinoti@student.moringaschool.com>
2025-12-10 10:20:27 +0000

    modified

--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Logo = () => {
   return (
     <Link to="/">
-      <div className="font-monoton text-3xl hover:text-red-800 cursor-pointer text-center transition">
+      <div className="font-monoton text-3xl hover:text-yellow-800 cursor-pointer text-center transition">
         SHEEBA
       </div>
       <div className="georgia text-3xl hover:text-black-800 cursor-pointer text-center transition">
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@ export const checkboxCategories = ["face","makeup", "body care","perfume","hair"
 
 export const gendersList = ["all", "men", "women", "unisex"];
 
-export const ratings = [1, 2, 3, 4];
+export const ratings = [1, 2, 3, 4, 5];
